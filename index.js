6acc473488e1906fa7865673a25cbeae0d470aed
sdalila <dalila.spahic@hotmail.com>
2019-03-06 18:18:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const sanitize = require("sanitize-filename");
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.set('view engine', 'ejs');
 
