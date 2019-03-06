46710848b54c82083ab4fa129b8e864106af92e7
sdalila <dalila.spahic@hotmail.com>
2019-03-06 16:57:19 +0100

    root files

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fs = require('fs');
+const sanitize = require("sanitize-filename");
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/files', (_, res) => {
+    const files = fs.readdirSync(".").map(file => { return {
+        filename: file,
+        date: fs.statSync(file).birthtime
+    }});
+    res.render('files', {
+        files: files.filter(file => file.filename.endsWith('.csv'))
+    });
+});
+
+app.get('/getfile/:filename', (req, res) => {
+    if (req.params.filename && req.params.filename.endsWith('.csv')) {
+        res.sendFile(__dirname + "/" + req.params.filename);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.use(bodyParser.text({
+    type: ["text/csv", "application/csv"]
+}));
+
+app.post('/addcsv', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    try {
+        const timestamp = (new Date()).toJSON();
+        fs.writeFileSync(sanitize(timestamp + ".csv"), req.body.toString());
+        res.send();
+    } catch(err)
+    {
+        res.sendStatus(500);
+    }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
